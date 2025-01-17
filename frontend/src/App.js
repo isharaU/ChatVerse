17005332614505ce377fab09540bffaa7ef28cec
isharaU <isharauditha5@gmail.com>
2025-01-18 00:23:06 +0530

    Add Chatpage component and set up routing in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route}  from 'react-router-dom';
 import Homepage from './Pages/Homepage';
+import Chatpage from './Pages/Chatpage';
 
 const App = () => {
   return (
     <div className='App'>
-      <Route path="/" Component={Homepage}/>
-      {/* <Homepage /> */}
+      <Route path="/" Component={Homepage} exact />
+      <Route path="/chats" Component={Chatpage}/>
     </div>
   );
 }
