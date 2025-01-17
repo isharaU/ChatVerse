fe01acf50a8e3273584953718daa1f9817cbd1f5
isharaU <isharauditha5@gmail.com>
2025-01-17 12:48:50 +0530

    Set up express server to listen on port 5000

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+const express = require('express'); // Import express
+
+const app = express(); // Create an express app
+
+app.listen(5000,console.log('Server is running on port 5000')); // Listen on port 5000
