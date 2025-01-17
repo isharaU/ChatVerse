1cff54d1fdba14f1cdd10dc27c033a0bcec03c30
isharaU <isharauditha5@gmail.com>
2025-01-18 00:19:46 +0530

    Add Homepage component and set up routing in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {Route}  from 'react-router-dom';
+import Homepage from './Pages/Homepage';
 
 const App = () => {
-  return <div>Hello, ChatVerse!</div>;
-};
+  return (
+    <div className='App'>
+      <Route path="/" Component={Homepage}/>
+      {/* <Homepage /> */}
+    </div>
+  );
+}
 
 export default App;
