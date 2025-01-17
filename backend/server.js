eaabc97315bc1ffbbc1b30a9b1970973b80e0893
isharaU <isharauditha5@gmail.com>
2025-01-17 19:59:18 +0530

    Add dotenv configuration and update server port handling

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express'); // Import express
 const {chats} = require('./data/data'); // Import chats data
+const dotenv = require('dotenv'); // Import dotenv
 
 const app = express(); // Create an express app
+dotenv.config(); // Configure dotenv
 
 app.get('/',(req,res) => { // Create a route for the home page
     res.send('API is Running'); // Send a response to the client
@@ -11,4 +13,6 @@ app.get('/api/chat',(req,res) => {
     res.send(chats); 
 });
 
-app.listen(5000,console.log('Server is running on port 5000')); // Listen on port 5000
+const PORT = process.env.PORT || 5000; // Define the port
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
