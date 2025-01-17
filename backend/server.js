332e9ac935898e87b38f9c6d0dda0c82813c58cc
isharaU <isharauditha5@gmail.com>
2025-01-17 16:16:24 +0530

    Add home route and chatverse dependency to backend

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,8 @@ const express = require('express'); // Import express
 
 const app = express(); // Create an express app
 
+app.get('/',(req,res) => { // Create a route for the home page
+    res.send('API is Running'); // Send a response to the client
+});
+
 app.listen(5000,console.log('Server is running on port 5000')); // Listen on port 5000
