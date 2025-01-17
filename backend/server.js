ed8f9ae77b008f9d962bc30d18037c4d63ccde4f
isharaU <isharauditha5@gmail.com>
2025-01-17 18:07:18 +0530

    Add chat data and API endpoint for retrieving chats

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express'); // Import express
+const {chats} = require('./data/data'); // Import chats data
 
 const app = express(); // Create an express app
 
 app.get('/',(req,res) => { // Create a route for the home page
     res.send('API is Running'); // Send a response to the client
+});
+
+app.get('/api/chat',(req,res) => { 
+    res.send(chats); 
 });
 
 app.listen(5000,console.log('Server is running on port 5000')); // Listen on port 5000
