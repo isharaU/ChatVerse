61cfb263fb46781b1e69de812dc4e980f484b3aa
isharaU <isharauditha5@gmail.com>
2025-01-19 23:23:55 +0530

    Add allUsers endpoint to fetch users based on search query

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const generateToken = require('../config/generateToken');
+const { request } = require('express');
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password, pic } = req.body;
@@ -60,4 +61,21 @@ const authUser = asyncHandler(async (req, res) => {
 
 });
 
-module.exports = { registerUser, authUser };
+// /api/user?search=ishara
+const allUsers = asyncHandler(async (req, res) => {
+    const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+    res.send(users);
+});
+
+                             
+
+module.exports = { registerUser, authUser, allUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { registerUser } = require('../controllers/userControllers');
-const { authUser } = require('../controllers/userControllers');
+const { authUser, allUsers } = require('../controllers/userControllers');
 
 const router = express.Router();
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(allUsers)
 router.post('/login', authUser)
 
 module.exports = router;
